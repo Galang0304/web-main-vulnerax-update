c60d1aec67de4ce6913ae68e3fef4915cc44aff9
mamanwhide <abdulvivo52@gmail.com>
2025-04-27 23:21:42 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,56 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-};
-
-export default nextConfig;
+    async headers() {
+      return [
+        {
+          // Terapkan header ke semua rute
+          source: '/:path*',
+          headers: [
+            {
+              key: 'Cross-Origin-Opener-Policy',
+              value: 'same-origin',
+            },
+            {
+              key: 'Cross-Origin-Embedder-Policy',
+              value: 'require-corp',
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'Permissions-Policy',
+              value: 'geolocation=(), camera=(), microphone=()',
+            },
+            {
+              key: 'Cross-Origin-Resource-Policy',
+              value: 'same-origin',
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'strict-origin-when-cross-origin',
+            },
+            {
+              key: 'X-Permitted-Cross-Domain-Policies',
+              value: 'none',
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Content-Security-Policy',
+              value: "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; font-src 'self'; connect-src 'self'; frame-src 'none'; object-src 'none';",
+            },
+            {
+              key: 'Strict-Transport-Security',
+              value: 'max-age=31536000; includeSubDomains; preload',
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
